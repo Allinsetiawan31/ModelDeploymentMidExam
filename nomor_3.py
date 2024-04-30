9fb3683a01bea67661b2c24bbe264e08cd3acea6
Allinsetiawan31 <115355847+Allinsetiawan31@users.noreply.github.com>
2024-04-30 15:44:24 +0700

    Update nomor_3.py

--- nomor_3.py
+++ nomor_3.py
@@ -7,8 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1IjTRCRcyaiq8fuXcfUf1lQfACF495xKX
 """
 
-pip install streamlit
-
 import streamlit as st
 import joblib
 import numpy as np
